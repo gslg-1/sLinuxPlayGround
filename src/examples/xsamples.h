3758cc739de0e01a6731954e8225dc0090f591d4
gslg-1 <61031185+gslg-1@users.noreply.github.com>
2021-11-06 17:56:22 +0100

    First Example

---
+++ src/examples/xsamples.h
@@ -0,0 +1,6 @@
+#ifndef SRC_EXAMPLS_XSAMPLES_H
+#define SRC_EXAMPLS_XSAMPLES_H
+
+int xsample1_run(void);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,15 @@
+
+/* System includes */
+
+/* Project includes */
+#include "examples/xsamples.h"
+
+/* Function Prototypes ------------------------------------- */
+int main(int argc, char **args);
+
+/* Function Bodies -----------------------------------------  */
+int main(int argc, char **args)
+{
+  int i_result = xsample1_run();
+  return i_result;
+}
